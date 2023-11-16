9b92bfc80e088c5ec555be7c23113058f3c2a429
C410lol <gcaio7463@gmail.com>
2023-11-16 18:15:20 -0300

    Setting the endDate when the user is finalizing his notebook

--- src/main/java/com/api/notebook/services/NotebookService.java
+++ src/main/java/com/api/notebook/services/NotebookService.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
 
 @Service
@@ -64,6 +66,7 @@ public class NotebookService {
             var byteArrayResource = new ByteArrayResource(byteArrayOutput.toByteArray());
 
             notebook.get().setStatus(StatusEnum.OFF);
+            notebook.get().setEndDate(LocalDate.now(ZoneId.of("UTC-3")));
             saveNotebook(notebook.get());
 
             return byteArrayResource;
