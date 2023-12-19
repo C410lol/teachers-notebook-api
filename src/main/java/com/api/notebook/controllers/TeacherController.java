3f295514a9ac67ab8a9ef6da80e1063699c37fe7
C410lol <gcaio7463@gmail.com>
2023-12-19 13:54:36 -0300

    Adding some code to send and email to the user when he does some actions

--- src/main/java/com/api/notebook/controllers/TeacherController.java
+++ src/main/java/com/api/notebook/controllers/TeacherController.java
@@ -1,11 +1,17 @@
 package com.api.notebook.controllers;
 
 import com.api.notebook.enums.RoleEnum;
+import com.api.notebook.enums.VerificationCodeEnum;
 import com.api.notebook.models.AuthModel;
+import com.api.notebook.models.EmailModel;
 import com.api.notebook.models.dtos.TeacherDto;
+import com.api.notebook.models.dtos.TeacherWithoutPasswordDto;
 import com.api.notebook.models.entities.TeacherEntity;
+import com.api.notebook.models.entities.VerificationCodeEntity;
+import com.api.notebook.producers.MailProducer;
 import com.api.notebook.services.JwtService;
 import com.api.notebook.services.TeacherService;
+import com.api.notebook.services.VerificationCodeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +22,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+import java.util.Random;
 import java.util.UUID;
 
 @RestController
@@ -25,14 +33,31 @@ public class TeacherController {
 
     private final TeacherService teacherService;
     private final JwtService jwtService;
+    private final VerificationCodeService verificationCodeService;
+    private final MailProducer mailProducer;
+    private final Random random;
 
     @PostMapping("/create") //POST endpoint to create a teacher entity
-    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
     public ResponseEntity<Object> createTeacher(@RequestBody @Valid TeacherDto teacherDto) {
         var teacherEntity = new TeacherEntity();
         BeanUtils.copyProperties(teacherDto, teacherEntity);
-        teacherEntity.setRole(RoleEnum.ROLE_USER);
-        teacherService.saveTeacher(teacherEntity);
+        var createdTeacher = teacherService.createTeacher(teacherEntity);
+
+        var code = random.nextInt(1000, 9999);
+        var verificationCodeEntity = new VerificationCodeEntity();
+        verificationCodeEntity.setCode(code);
+        verificationCodeEntity.setType(VerificationCodeEnum.EMAIL_VERIFICATION);
+        teacherService.setVerificationCodeToTeacher(createdTeacher.getId(), verificationCodeEntity);
+        verificationCodeService.save(verificationCodeEntity);
+
+        mailProducer.sendMailMessage(new EmailModel(
+                teacherEntity.getEmail(),
+                "Confirme Sua Conta No Site TeacherNotesHub!",
+                String.format("Clique neste link para confirmar sua conta ou copie e cole no seu navegador: " +
+                        "https://app.teachernoteshub.online/verify-account/%s?vCode=%s",
+                        teacherEntity.getId(), code)
+        ));
+
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
@@ -43,7 +68,6 @@ public class TeacherController {
     }
 
     @GetMapping("/{teacherId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
     public ResponseEntity<Object> getTeacherById(@PathVariable(value = "teacherId") UUID teacherId) {
         var teacherOptional = teacherService.findTeacherById(teacherId);
         if (teacherOptional.isPresent()) {
@@ -51,32 +75,96 @@ public class TeacherController {
             if (authenticationId.equals(teacherId)) {
                 return ResponseEntity.ok(teacherOptional.get());
             }
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado!");
     }
 
     @PutMapping("/{teacherId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
     public ResponseEntity<Object> editTeacher(@PathVariable(value = "teacherId") UUID teacherId,
-                                              @RequestBody @Valid TeacherDto teacherDto) {
+                                              @RequestBody @Valid TeacherWithoutPasswordDto teacherWithoutPasswordDto) {
         var teacherOptional = teacherService.findTeacherById(teacherId);
         if (teacherOptional.isPresent()) {
             var authenticationId = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             if (authenticationId.equals(teacherId)) {
                 var teacherEntity = new TeacherEntity();
                 BeanUtils.copyProperties(teacherOptional.get(), teacherEntity);
-                BeanUtils.copyProperties(teacherDto, teacherEntity);
-                teacherService.saveTeacher(teacherEntity);
+                BeanUtils.copyProperties(teacherWithoutPasswordDto, teacherEntity);
+                teacherService.editTeacher(teacherEntity);
                 return ResponseEntity.ok().build();
             }
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado!");
     }
 
+    @PostMapping("/request-password-change")
+    public ResponseEntity<Object> requestPasswordChange(
+            @RequestParam(value = "id", required = false) UUID userId,
+            @RequestParam(value = "email", required = false) String userEmail
+    ) {
+        Optional<TeacherEntity> teacherOptional;
+
+        if (userId != null) {
+            teacherOptional = teacherService.findTeacherById(userId);
+        } else if (userEmail != null) {
+            teacherOptional = teacherService.findTeacherByEmail(userEmail);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        if (teacherOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        var code = random.nextInt(1000, 9999);
+        var verificationCodeEntity = new VerificationCodeEntity();
+        verificationCodeEntity.setCode(code);
+        verificationCodeEntity.setType(VerificationCodeEnum.PASSWORD_CHANGE);
+        teacherService.setVerificationCodeToTeacher(teacherOptional.get().getId(), verificationCodeEntity);
+        verificationCodeService.save(verificationCodeEntity);
+
+        mailProducer.sendMailMessage(new EmailModel(
+                teacherOptional.get().getEmail(),
+                "Requisição Para Trocar de Senha No Site TeacherNotesHub.",
+                String.format("Clique neste link para mudar sua senha ou copie e cole no seu navegador: " +
+                                "https://app.teachernoteshub.online/change-password/%s?vCode=%s",
+                        teacherOptional.get().getId(), code)
+        ));
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{teacherId}/change-password")
+    public ResponseEntity<Object> editTeacherPassword(
+            @PathVariable(value = "teacherId") UUID teacherId,
+            @RequestParam(value = "vCode") Integer vCode,
+            @RequestBody String newPassword
+    ) {
+        var teacherOptional = teacherService.findTeacherById(teacherId);
+        if (teacherOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        var vCodeOptional = verificationCodeService.findByTeacherId(teacherId, VerificationCodeEnum.PASSWORD_CHANGE);
+        if (vCodeOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        if (!vCodeOptional.get().getCode().equals(vCode)) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        teacherOptional.get().setPassword(newPassword);
+        teacherService.createTeacher(teacherOptional.get());
+
+
+        verificationCodeService.deleteById(vCodeOptional.get().getId());
+
+        return ResponseEntity.ok().build();
+    }
+
     @DeleteMapping("/{teacherId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
     public ResponseEntity<Object> deleteTeacher(@PathVariable(value = "teacherId") UUID teacherId) {
         var teacherOptional = teacherService.findTeacherById(teacherId);
         if (teacherOptional.isPresent()) {
@@ -85,30 +173,96 @@ public class TeacherController {
                 teacherService.deleteTeacherById(teacherId);
                 return ResponseEntity.ok().build();
             }
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado!");
     }
 
+    @PutMapping("/verify-account/{teacherId}")
+    public ResponseEntity<Object> verifyTeacherAccount(
+            @PathVariable(value = "teacherId") UUID teacherId,
+            @RequestParam(value = "vCode") Integer vCode
+    ) {
+        var teacherOptional = teacherService.findTeacherById(teacherId);
+        if (teacherOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        var verificationCodeOptional = verificationCodeService.findByTeacherId(
+                teacherId, VerificationCodeEnum.EMAIL_VERIFICATION);
+        if (verificationCodeOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        if (!verificationCodeOptional.get().getCode().equals(vCode)) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        teacherOptional.get().setRole(RoleEnum.ROLE_USER);
+        teacherService.editTeacher(teacherOptional.get());
+
+        verificationCodeService.deleteById(verificationCodeOptional.get().getId());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/resend-verification-email")
+    public ResponseEntity<Object> resendVerificationEmail(
+            @RequestParam(value = "teacherId", required = false) UUID teacherId,
+            @RequestParam(value = "teacherEmail", required = false) String email
+    ) {
+        Optional<TeacherEntity> teacherOptional;
+
+        if (teacherId != null) {
+            teacherOptional = teacherService.findTeacherById(teacherId);
+        } else if (email != null) {
+            teacherOptional = teacherService.findTeacherByEmail(email);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        if (teacherOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        var verificationCodeOptional = verificationCodeService.findByTeacherId(
+                teacherOptional.get().getId(), VerificationCodeEnum.EMAIL_VERIFICATION);
+        if (verificationCodeOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        mailProducer.sendMailMessage(new EmailModel(
+                teacherOptional.get().getEmail(),
+                "Confirme Sua Conta No Site TeacherNotesHub!",
+                String.format("Clique neste link para confirmar sua conta ou copie e cole no seu navegador: " +
+                                "https://app.teachernoteshub.online/verify-account/%s?vCode=%s",
+                        teacherId, verificationCodeOptional.get().getCode())
+        ));
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{teacherId}/verified")
+    public ResponseEntity<Object> isUserVerified(
+            @PathVariable(value = "teacherId") UUID teacherId
+    ) {
+        var teacherOptional = teacherService.findTeacherById(teacherId);
+        if (teacherOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(false);
+        }
+        if (teacherOptional.get().getRole() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(false);
+        }
+        return ResponseEntity.ok(true);
+    }
+
     @PostMapping("/login") //POST endpoint to authenticate a user
-    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
     public ResponseEntity<Object> authenticateUser(@RequestBody @Valid AuthModel authModel) {
         var authentication = teacherService.tryToAuthenticateTeacher(authModel, jwtService); //Try to authenticate user
         if (authentication != null) {
             return ResponseEntity.ok(authentication); //If success it returns a token
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email ou senha incorretos!");
-    }
-
-    @PostMapping("/get-by-token") //Get endpoint to get user by token
-    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
-    public ResponseEntity<Object> getUserByToken(@RequestBody @NotNull String token) {
-        var userEmail = jwtService.getEmailByToken(token.substring(7));
-        var teacherOptional = teacherService.findTeacherByEmail(userEmail);
-        if (teacherOptional.isPresent()) {
-            return ResponseEntity.ok(teacherOptional.get());
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado!");
     }
 
 }
--- src/main/java/com/api/notebook/services/TeacherService.java
+++ src/main/java/com/api/notebook/services/TeacherService.java
@@ -1,8 +1,10 @@
 package com.api.notebook.services;
 
 import com.api.notebook.models.AuthModel;
+import com.api.notebook.models.AuthReturnModel;
 import com.api.notebook.models.entities.NotebookEntity;
 import com.api.notebook.models.entities.TeacherEntity;
+import com.api.notebook.models.entities.VerificationCodeEntity;
 import com.api.notebook.repositories.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -20,10 +22,12 @@ public class TeacherService {
     private final TeacherRepository teacherRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void saveTeacher(@NotNull TeacherEntity teacher) {
-        if (teacher.getId() == null) { //Checks if this entity doesn't exist, if so it encodes its password
-            teacher.setPassword(passwordEncoder.encode(teacher.getPassword()));
-        }
+    public TeacherEntity createTeacher(@NotNull TeacherEntity teacher) {
+        teacher.setPassword(passwordEncoder.encode(teacher.getPassword()));
+        return teacherRepository.save(teacher);
+    }
+
+    public void editTeacher(TeacherEntity teacher) {
         teacherRepository.save(teacher);
     }
 
@@ -44,16 +48,24 @@ public class TeacherService {
     }
 
     //Try to authenticate teacher
-    public String tryToAuthenticateTeacher(@NotNull AuthModel authModel, JwtService jwtService) {
+    public AuthReturnModel tryToAuthenticateTeacher(@NotNull AuthModel authModel, JwtService jwtService) {
         var teacherOptional = findTeacherByEmail(authModel.getEmail());
         if (teacherOptional.isPresent()) { //Verify if the teacher exists
 
             //Verify if the auth model password matches the found teacher entity
             if (passwordEncoder.matches(authModel.getPassword(), teacherOptional.get().getPassword())) {
-                return jwtService.generateToken(teacherOptional.get().getEmail()); //If success it returns a token
+                var token = jwtService.generateToken(teacherOptional.get().getEmail());
+                return new AuthReturnModel(
+                        teacherOptional.get().getId(),
+                        token);
             }
         }
         return null;
+    }
+
+    public void setVerificationCodeToTeacher(UUID teacherId, @NotNull VerificationCodeEntity verificationCode) {
+        var teacherOptional = findTeacherById(teacherId);
+        teacherOptional.ifPresent(verificationCode::setTeacher);
     }
 
     public void setNotebookToTeacher(UUID teacherId, @NotNull NotebookEntity notebook) { //Set notebook to a teacher
