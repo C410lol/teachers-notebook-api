5ffe346c0acb854c391a7030679104edd1ebc08b
C410lol <gcaio7463@gmail.com>
2024-02-01 13:59:26 -0300

    Deleting some classes

--- src/main/java/com/api/notebook/enums/ClassEnum.java
+++ src/main/java/com/api/notebook/enums/ClassEnum.java
@@ -2,21 +2,15 @@ package com.api.notebook.enums;
 
 public enum ClassEnum {
 
-    Ensino_Fundamental_5_A,
-    Ensino_Fundamental_5_B,
     Ensino_Fundamental_6_A,
     Ensino_Fundamental_6_B,
     Ensino_Fundamental_7_A,
     Ensino_Fundamental_7_B,
     Ensino_Fundamental_8_A,
-    Ensino_Fundamental_8_B,
     Ensino_Fundamental_9_A,
     Ensino_Fundamental_9_B,
     Ensino_Médio_1_A,
-    Ensino_Médio_1_B,
     Ensino_Médio_2_A,
-    Ensino_Médio_2_B,
-    Ensino_Médio_3_A,
-    Ensino_Médio_3_B;
+    Ensino_Médio_3_A
     
 }
