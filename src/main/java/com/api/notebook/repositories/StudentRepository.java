fa369342090cd3db672eff0ff92d3378a138c539
C410lol <gcaio7463@gmail.com>
2024-05-28 14:45:52 -0300

    Adding code to get the students by the 'institutionId'

--- src/main/java/com/api/notebook/repositories/StudentRepository.java
+++ src/main/java/com/api/notebook/repositories/StudentRepository.java
@@ -3,6 +3,8 @@ package com.api.notebook.repositories;
 import com.api.notebook.enums.ClassEnum;
 import com.api.notebook.models.entities.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +13,25 @@ import java.util.UUID;
 @Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
 
+    @Query(
+            value = "SELECT * FROM students " +
+                    "WHERE institution_id = :institutionId " +
+                    "AND " +
+                    "classe = :classe",
+            nativeQuery = true
+    )
+    List<StudentEntity> findAllByInstitutionIdAndClasse(
+            @Param(value = "institutionId") UUID institutionId,
+            @Param(value = "classe") String classe
+    );
+
     List<StudentEntity> findAllByClasse(ClassEnum classEnum);
+
+    @Query(
+            value = "SELECT COUNT(*) AS total_count FROM students " +
+                    "WHERE classe = :classe",
+            nativeQuery = true
+    )
+    int getSizeOfStudentsByClasse(@Param(value = "classe") String classe);
 
 }
